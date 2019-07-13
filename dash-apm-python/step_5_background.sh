d2558630b628b92c1bc9499614921bfff0e1144b
brettlangdon <brett.langdon@datadoghq.com>
2019-07-13 18:14:15 -0400

    fix env variable

--- dash-apm-python/step_5_background.sh
+++ dash-apm-python/step_5_background.sh
@@ -3,4 +3,4 @@
 echo "DD_LOGS_ENABLED=true" >> /tracing-workshop/.env-agent
 echo "DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL=true" >> /tracing-workshop/.env-agent
 
-echo "DD_LOGS_INTEGRATION=true" >> /tracing-workshop/.env
+echo "DD_LOGS_INJECTION=true" >> /tracing-workshop/.env
