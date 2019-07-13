06ce3871d4fdf3fc203b9660bebc5edd205b5ce6
brettlangdon <brett.langdon@datadoghq.com>
2019-07-13 09:06:04 -0400

    use .env file for adding errors and latency

--- dash-apm-python/background.sh
+++ dash-apm-python/background.sh
@@ -5,8 +5,5 @@ mkdir /tracing-workshop
 git clone https://github.com/brettlangdon/distributed-tracing-with-apm-workshop /tracing-workshop
 cd /tracing-workshop
 
-# Try to install yaml_cli helper
-(cd /tmp/ && git clone https://github.com/Gallore/yaml_cli && cd ./yaml_cli && python setup.py install) || true
-
 # Start pulling containers as soon as they load
 docker-compose pull
--- dash-apm-python/step_5_background.sh
+++ dash-apm-python/step_5_background.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-command -v yaml_cli > /dev/null && \
-    yaml_cli -i /tracing-workshop/docker-compose.yml \
-             -o /tracing-workshop/docker-compose.yml \
-             --list-append \
-             -s services:frontend:environment 'WORKSHOP_ADD_ERRORS=true'
+echo "WORKSHOP_ADD_ERRORS=true" >> /tracing-workshop/.env
