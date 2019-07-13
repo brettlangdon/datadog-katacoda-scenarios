e18488063415239ab48dad03d0c163c32ed194bc
brettlangdon <brett.langdon@datadoghq.com>
2019-07-12 22:10:57 -0400

    use absolute path

--- dash-apm-python/step_6_background.sh
+++ dash-apm-python/step_6_background.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 command -v yaml_cli > /dev/null && \
-    yaml_cli -i docker-compose.yml \
-             -o docker-compose.yml \
+    yaml_cli -i /tracing-workshop/docker-compose.yml \
+             -o /tracing-workshop/docker-compose.yml \
              --list-append \
              -s services:sensors:environment 'WORKSHOP_ADD_LATENCY=true'
